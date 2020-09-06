0151e11fb847a6918d6061dee24036db2928f45c
Niklas Freese <niklas.freese@ewetel.net>
2020-09-06 13:37:19 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Http Utils
 Basic Utils for working with HTTP in PHP.
+
+## Response Dumper
+Dump PSR-7 Respones into the output stream.
+
+```php
+<?php
+
+use Nyrados\Http\Utils\ResponseDumper;
+
+$dump = new ResponseDumper($response);
+
+// Usage:
+$dump->dumpHeaders();
+$dump->dumpBody();
+
+// Or:
+$dump->dump();
+```
--- src/ResponseDumper.php
+++ src/ResponseDumper.php
@@ -5,121 +5,94 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 
 /**
- * Class For Writing PSR-7 Response
+ * Class for dumping PSR-7 response
  */
 
 class ResponseDumper
 {
-	
-	/** @var ResponseInterface */
-	private $response;
-	
-	private $end;
-	private $start = 0;
-	private $sentHeaders = false;
-	private $streamed = false;
-	
-	public function __construct(ResponseInterface $response)
-	{
-		$this->response = $response;
+    
+    /** @var ResponseInterface */
+    private $response;
+    
+    private $end;
+    private $start = 0;
+    private $sentHeaders = false;
+    private $streamed = false;
+    
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
 
-        if($this->response->hasHeader('Content-Range')) {
+        if ($this->response->hasHeader('Content-Range')) {
             preg_match('/(\d+)-(\d+)\/(\d+)/', $this->response->getHeaderLine('Content-Range'), $data);
-            $this->start = $data[1]; 
-            $this->end = $data[2];  
+            $this->start = $data[1];
+            $this->end = $data[2];
         } else {
-			$this->end = $this->response->getBody()->getSize() - 1;
-		}
-	}
-	
-	/**
-	 * Writes given Headers & Response Code
-	 *
-	 * @param boolean $contentLength for set $contentLength Header for Body
-	 * @return void
-	 */
-	public function dumpHeaders($contentLength = true): void
-	{
-		if (!$this->sentHeaders) {
-			
-			foreach($this->response->getHeaders() as $name => $values) {
-				foreach($values as $value) {
-					header($name . ': ' . $value, count($values) > 1);
-				}
-			}
-
-			header('HTTP/' . $this->response->getProtocolVersion() . ' ' . $this->response->getStatusCode() . ' ' . $this->response->getReasonPhrase());
-			
-			if($contentLength) {
-				$stream = $this->response->getBody();
-				header('Content-Length: ' . $stream->getSize());
-			}	
-
-			$this->sentHeaders = true;		
-		}
-	}
-	
-	/**
-	 * Dumps Headers & Writes Content
-	 *
-	 * @param boolean $contentLength see dumpHeaders()
-	 * @return void
-	 */
-    public function dump(bool $contentLength = true): void
-    { 
-		$this->dumpHeaders($contentLength);
-		$this->dumpStream();
-
+            $this->end = $this->response->getBody()->getSize() - 1;
+        }
     }
-	
-	/**
-	 * Dumps Stream and Outputs it directly
-	 *
-	 * @return void
-	 */
-	public function dumpStream(): void
-	{
-		if (!$this->streamed) {
-			
-			$stream = $this->response->getBody();
+    
+    /**
+     * Writes given Headers & Response Code
+     *
+     * @param boolean $contentLength for set $contentLength Header for Body
+     * @return void
+     */
+    public function dumpHeaders($contentLength = true): void
+    {
+        if (!$this->sentHeaders) {
+            foreach ($this->response->getHeaders() as $name => $values) {
+                foreach ($values as $value) {
+                    header($name . ': ' . $value, count($values) > 1);
+                }
+            }
 
-			//Dump Stream
-			$buffer = 1024 * 8;
-			$stream->seek($this->start);
-			set_time_limit(0);
-			while(!$stream->eof() && ($p = $stream->tell()) <= $this->end) {
-				if ($p + $buffer > $this->end) {
-					$buffer = $this->end - $p + 1;
-				}
+            header('HTTP/' . $this->response->getProtocolVersion() . ' ' . $this->response->getStatusCode() . ' ' . $this->response->getReasonPhrase());
+            
+            if ($contentLength) {
+                $stream = $this->response->getBody();
+                header('Content-Length: ' . $stream->getSize());
+            }
 
-				echo $stream->read($buffer);
-				flush();
-			}
-			$stream->close(); 		
-		}
-	}
+            $this->sentHeaders = true;
+        }
+    }
+    
+    /**
+     * Dumps Headers & Writes Content
+     *
+     * @param boolean $contentLength see dumpHeaders()
+     * @return void
+     */
+    public function dump(): void
+    {
+        $this->dumpHeaders($contentLength);
+        $this->dumpBody();
+    }
+    
+    /**
+     * Dumps Body and Outputs it directly
+     *
+     * @return void
+     */
+    public function dumpBody(): void
+    {
+        if (!$this->streamed) {
+            $stream = $this->response->getBody();
 
-	/**
-	 * For Invoke Access, see dump()
-	 *
-	 * @param boolean $contentLength
-	 * @return void
-	 */
-	public function __invoke(bool $contentLength = true)
-	{
-		$this->dump($contentLength);
-	}
+            //Dump Stream
+            $buffer = 1024 * 8;
+            $stream->seek($this->start);
+            set_time_limit(0);
+            while (!$stream->eof() && ($p = $stream->tell()) <= $this->end) {
+                if ($p + $buffer > $this->end) {
+                    $buffer = $this->end - $p + 1;
+                }
 
-	/**
-	 * For static access, see dunp()
-	 *
-	 * @param ResponseInterface $response
-	 * @param boolean $contentLength
-	 * @return void
-	 */
-	public static function render(ResponseInterface $response, bool $contentLength = true): void
-	{
-		$self = new self($response);
-		$self->dump($contentLength);
-	}
+                echo $stream->read($buffer);
+                flush();
+            }
+            $stream->close();
+        }
+    }
 }
