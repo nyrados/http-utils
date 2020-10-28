be8c0a4e9dfa62c12ccface16d51233edc34f48c
Niklas Freese <niklas.freese@ewetel.net>
2020-10-28 21:19:28 +0100

    feat: MiddlewareWrapper & NullMiddleware

---
+++ src/Middleware/MiddlewareWrapper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Nyrados\Http\Utils\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class MiddlewareAdapter implements MiddlewareInterface
+{
+    private $middleware;
+
+    public function __construct(callable $middleware)
+    {
+        $this->middleware = $middleware;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return ($this->middleware)($request, $handler->handle($request));
+    }
+}
